7c2db56e1fc2f4dbe820d9bbcbccd081e58212e5
sara <saraelallouche@hotmail.fr>
2022-12-14 16:01:55 +0100

    add component gif

--- ex2/src/app/app.module.ts
+++ ex2/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { FooterComponent } from './footer/footer.component';
+import { GifComponent } from './gif/gif.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    FooterComponent
+    FooterComponent,
+    GifComponent
   ],
   imports: [
     BrowserModule,
