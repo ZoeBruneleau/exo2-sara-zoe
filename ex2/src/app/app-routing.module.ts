11a11fefb49e2aa992a65be8b5f43da75da3159a
ZoeBruneleau <92059910+ZoeBruneleau@users.noreply.github.com>
2022-12-16 10:14:49 +0100

    route liste

--- ex2/src/app/app-routing.module.ts
+++ ex2/src/app/app-routing.module.ts
@@ -2,11 +2,14 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {ContactComponent} from "./contact/contact.component";
 import {GifComponent} from "./gif/gif.component";
+import {ListeComponent} from "./liste/liste.component";
 
 const routes: Routes = [
   { path : 'contact', component:ContactComponent },
-  { path: 'home', component: GifComponent },
-  { path: '**', component:GifComponent}
+  { path : 'home', component: GifComponent },
+  { path : 'liste', component:ListeComponent  },
+  { path : '**', component:GifComponent },
+
 ];
 
 @NgModule({
